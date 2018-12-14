75e73ad8eef571d4c90560e3c5817db76c9e7ca4
Jan Kowalski <jan@o2.pl>
2018-12-14 11:46:26 +0100

    ZTAI_1 skonczone

--- api/app/business/business.container.js
+++ api/app/business/business.container.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import postManager from './post.manager';
+import userManager from './user.manager';
 
 function getContext(request) {
   return { user: request && request.user };
@@ -16,6 +17,7 @@ const createBusinessContainer = (request, config) => {
 
   return {
     getPostManager: getter(postManager, request),
+    getUserManager: getter(userManager)
   };
 };
 
